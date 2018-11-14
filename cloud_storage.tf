2ae19b05f22a1f563228aa3720de393735a8983a
jkwong888 <jkwong@ca.ibm.com>
2018-11-14 12:38:39 -0500

    additional configs to the bucket

--- cloud_storage.tf
+++ cloud_storage.tf
@@ -30,3 +30,35 @@ resource "google_storage_bucket" "icp-config" {
   name   = "icp-config-${random_id.clusterid.hex}"
   storage_class = "REGIONAL"
 }
+
+/*
+TODO
+resource "google_storage_bucket_object" "icp-router-crt" {
+  name    = "cfc-certs/icp-router.crt"
+  bucket  = "${google_storage_bucket.icp-config.name}"
+  source  = "${path.module}/cfc-certs/icp-router.crt"
+}
+
+resource "google_storage_bucket_object" "icp-router-key" {
+  name    = "cfc-certs/icp-router.key"
+  bucket  = "${google_storage_bucket.icp-config.name}"
+  source  = "${path.module}/cfc-certs/icp-router.key"
+}
+*/
+
+resource "google_storage_bucket_object" "cloud_provider_conf" {
+  name    = "misc/cloudprovider/cloud_provider_gce.conf"
+  bucket  = "${google_storage_bucket.icp-config.name}"
+  content = <<EOF
+[global]
+project-id = ${var.project}
+network-project-id = ${var.project}
+network-name = ${google_compute_network.icp_vpc.name}
+subnetwork-name = ${google_compute_subnetwork.icp_region_subnet.name}
+node-instance-prefix = ${var.instance_name}-${random_id.clusterid.hex}
+node-tags = icp-cluster-${random_id.clusterid.hex}
+regional = true
+multizone = true
+EOF
+
+}
